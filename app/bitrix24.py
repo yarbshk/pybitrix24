a741c722fd891b3d245a14a6dc9c21c9cdc17e0b
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-08-31 19:38:12 +0300

    Create call methods and shortcuts

--- app/bitrix24.py
+++ app/bitrix24.py
@@ -8,21 +8,22 @@ class Bitrix24(object):
 
     def __init__(self, domain, client_id, client_secret, access_token=None,
                  client_endpoint=None, expires_in=3600, refresh_token=None,
-                 scope=None, server_endpoint=None, user_id=None):
+                 scope='', server_endpoint=None, user_id=0, transport='json'):
         self.access_token = access_token
         self.client_id = client_id
-        self.client_endpoint = client_endpoint
+        self.client_endpoint = client_endpoint # Must endswith a slash
         self.client_secret = client_secret
-        self.domain = domain
-        self.expires_in = expires_in
+        self.domain = domain # Without protocol and an ending slash
+        self.expires_in = expires_in # Tokens expire in 1 hour by default
         self.refresh_token = refresh_token
-        self.scope = scope
-        self.server_endpoint = server_endpoint
-        self.user_id = user_id
+        self.scope = scope # The default value means all available scopes
+        self.server_endpoint = server_endpoint # Must endswith a slash
+        self.transport = transport # Allowable values are 'json' or 'xml'
+        self.user_id = user_id # The default value means current user
 
     def _get_oauth_endpoint(self, action, query=None):
         """
-        Build an OAuth URL with/out query parameters.
+        Builds an OAuth URL with/out query parameters.
         :param action: str Action name of an OAuth endpoint
         :param query: dict Query parameters
         :return: str OAuth endpoint
@@ -38,7 +39,8 @@ class Bitrix24(object):
 
     def get_authorize_endpoint(self, **extra_query):
         """
-        Build an authorize URL to request an authorization code from
+        Builds an authorize URL to request an authorization code from. See:
+        https://training.bitrix24.com/rest_help/oauth/app_authentication.php
         a remote server using a browser.
         :param extra_query: dict Additional query parameters
         :return: str Authorize endpoint
@@ -48,11 +50,12 @@ class Bitrix24(object):
             'client_id': self.client_id,
             'response_type': 'code'
         })
-        return self._get_oauth_endpoint('authorize', query)
+        endpoint = self._get_oauth_endpoint('authorize', query=query)
+        return endpoint
 
     def get_tokens(self):
         """
-        Return current access and refresh tokens of the instance.
+        Returns current access and refresh tokens of the instance.
         :return: dict Access and refresh tokens
         """
         return {
@@ -62,7 +65,7 @@ class Bitrix24(object):
 
     def _request_tokens(self, query):
         """
-        Request handler of an OAuth tokens endpoint.
+        The request handler of an OAuth tokens endpoint.
         :param query: dict Query parameters
         :return: dict Encoded response text
         """
@@ -73,7 +76,8 @@ class Bitrix24(object):
 
     def request_tokens(self, code, **extra_query):
         """
-        Request access and refresh tokens of a Bitrix24 server.
+        Requests access and refresh tokens of a Bitrix24 server. See:
+        https://training.bitrix24.com/rest_help/oauth/app_authentication.php
         :param code: str Authentication request code
         :param extra_query: dict Additional query parameters
         """
@@ -82,7 +86,8 @@ class Bitrix24(object):
             'client_id': self.client_id,
             'client_secret': self.client_secret,
             'code': code,
-            'grant_type': 'authorization_code'
+            'grant_type': 'authorization_code',
+            'scope': self.scope
         })
         result = self._request_tokens(query)
         self.access_token = result.get('access_token')
@@ -96,7 +101,7 @@ class Bitrix24(object):
 
     def refresh_tokens(self, **extra_query):
         """
-        Refresh class tokens by appropriate requested tokens.
+        Refreshes class tokens by appropriate requested tokens.
         :param extra_query: dict Additional query parameters
         """
         query = extra_query.copy()
@@ -110,5 +115,67 @@ class Bitrix24(object):
         self.access_token = result.get('access_token')
         self.refresh_token = result.get('refresh_token')
 
-    def call_method(self, name):
-        pass
+    def call_method(self, method, params=None):
+        """
+        Requests any Bitrix24 method with/out parameters. See:
+        https://training.bitrix24.com/rest_help/js_library/rest/callMethod.php
+        :param method: str Dot-noted method name
+        :param params: dict Request parameters
+        :return: dict Encoded response text
+        """
+        url = '{endpoint}{method}.{transport}'.format(
+            endpoint=self.client_endpoint,
+            method=method,
+            transport=self.transport
+        )
+        query = {'auth': self.access_token}
+        r = requests.post(url, json=params, params=query)
+        result = json.loads(r.text)
+        return result
+
+    def call_batch(self, calls, halt_on_error=False):
+        """
+        Groups many single methods into a request. Can include macros
+        to access the results of the previous calls in the batch. See:
+        https://training.bitrix24.com/rest_help/js_library/rest/callBatch.php
+        :param calls: dict Sub-methods with params
+        :param halt_on_error: bool Halt on error
+        :return: dict Encoded response text
+        """
+        result = self.call_method('batch', {
+            'cmd': calls,
+            'halt': halt_on_error
+        })
+        return result
+
+    def call_bind(self, event, handler, auth_type=None):
+        """
+        Installs a new event handler. See:
+        https://training.bitrix24.com/rest_help/general/event_bind.php
+        :param event: str Event name
+        :param handler: str Handler URL
+        :param auth_type: int User ID
+        :return: dict Encoded response text
+        """
+        result = self.call_method('event.bind', {
+            'auth_type': auth_type or self.user_id,
+            'event': event,
+            'handler': handler
+        })
+        return result
+
+    def call_unbind(self, event, handler, auth_type=None):
+        """
+        Uninstalls a previously installed event handler. See:
+        https://training.bitrix24.com/rest_help/general/event_unbind.php
+        :param event: str Event name
+        :param handler: str Handler URL
+        :param auth_type: int User ID
+        :return: dict Encoded response text
+        """
+        result = self.call_method('event.unbind', {
+            'auth_type': auth_type,
+            'event': event,
+            'handler': handler
+        })
+        return result
